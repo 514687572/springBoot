4e648a40f9ce4ff94de9e35bb0c1de77d8a95c26
安爷 <chenjunan@foxmail.com>
2017-07-28 18:05:45 +0800

    springBoot

--- spring-boot/src/main/java/com/beyond/net/web/TestController.java
+++ spring-boot/src/main/java/com/beyond/net/web/TestController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 	
-	@RequestMapping("/helloworld")
+	@RequestMapping("/")
 	public String hello(){
 		return "helloworld";
 	}
